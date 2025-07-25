1a91767f5dac7a1f2c112f275c77440181577965
urmee04 <urmee04sust@gmail.com>
2025-07-25 00:12:10 -0400

    add AlertBoxProps type and AlertType union for alert component

---
+++ src/components/AlertBox/AlertBox.tsx
@@ -0,0 +1,9 @@
+// types/index.ts
+export type AlertType = "success" | "error" | "warning" | "info";
+
+export interface AlertBoxProps {
+  type: AlertType;
+  message: string;
+  onClose?: () => void;
+  children?: React.ReactNode;
+}
